b74b21398c0c4a39c15ae2d8b6951da96a58c6b4
J.K.Roflol <tori.tuan@gmail.com>
2024-03-21 11:35:58 +0700

    re-init

---
+++ src/main/java/com/example/fisintern_spring/FisinternSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.fisintern_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FisinternSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FisinternSpringApplication.class, args);
+	}
+
+}
