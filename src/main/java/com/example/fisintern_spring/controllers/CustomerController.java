8ab01a981871b8dc54acae9c3f8bc27f8bd2e633
J.K.Roflol <tori.tuan@gmail.com>
2024-04-24 15:57:53 +0700

    complete basic customers functiions

--- src/main/java/com/example/fisintern_spring/controllers/CustomerController.java
+++ src/main/java/com/example/fisintern_spring/controllers/CustomerController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -33,10 +34,27 @@ public class CustomerController {
         return customerService.findCustomerById(id);
     }
 
-    @GetMapping(path = "/findbyname")
-    public @ResponseBody List<Customer> findCustomerByName(@RequestParam String name) {
+    @GetMapping(path = "/find")
+    public @ResponseBody List<Customer> findCustomerByName(@RequestParam String keyword) {
         // This returns a JSON or XML with the users
-        return customerRepository.findCustomerByName(name);
+        return customerRepository.findCustomer(keyword);
+    }
+
+    @GetMapping(path = "/findcompany")
+    public @ResponseBody List<Customer> findCompanyCustomer() {
+        return customerRepository.findCompanyCustomer();
+    }
+
+    @GetMapping(path = "/finddebt")
+    public @ResponseBody List<Customer> findDebtCustomer() {
+        return customerRepository.findDebtCustomer();
+    }
+
+    @GetMapping(path = "/findbirthday")
+    public @ResponseBody List<Customer> findBirthdayCustomer(@RequestParam String d1, @RequestParam String d2) {
+        LocalDate date1 = LocalDate.parse(d1);
+        LocalDate date2 = LocalDate.parse(d2);
+        return customerRepository.findBirthdayCustomer(date1, date2);
     }
 
     @PostMapping(path = "/add") // Map ONLY POST Requests
--- src/main/java/com/example/fisintern_spring/repositories/CustomerRepository.java
+++ src/main/java/com/example/fisintern_spring/repositories/CustomerRepository.java
@@ -3,14 +3,24 @@ package com.example.fisintern_spring.repositories;
 import com.example.fisintern_spring.models.Customer;
 import org.springframework.data.jpa.repository.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-    @Query("SELECT s FROM Customer s WHERE s.name LIKE %?1%")
-    List<Customer> findCustomerByName(String first_name);
+    @Query("SELECT s FROM Customer s WHERE s.name LIKE %?1% OR s.phone LIKE %?1% OR s.email LIKE %?1%")
+    List<Customer> findCustomer(String keyword);
+
+    @Query("SELECT s FROM Customer s WHERE s.company IS NOT NULL")
+    List<Customer> findCompanyCustomer();
+
+    @Query("SELECT s FROM Customer s WHERE s.debt > 0")
+    List<Customer> findDebtCustomer();
+
+    @Query("SELECT s FROM Customer s WHERE s.dob BETWEEN ?1 and ?2")
+    List<Customer> findBirthdayCustomer(LocalDate date1, LocalDate date2);
 }
 
 
