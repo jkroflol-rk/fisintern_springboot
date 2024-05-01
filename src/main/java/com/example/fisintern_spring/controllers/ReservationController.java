d7d995e2657d5948795187f5922dce563eba3bd8
J.K.Roflol <tori.tuan@gmail.com>
2024-05-01 23:08:25 +0700

    minor updates

--- src/main/java/com/example/fisintern_spring/controllers/ReservationController.java
+++ src/main/java/com/example/fisintern_spring/controllers/ReservationController.java
@@ -1,4 +1,25 @@
 package com.example.fisintern_spring.controllers;
 
+import com.example.fisintern_spring.models.Reservation;
+import com.example.fisintern_spring.repositories.ReservationRepository;
+import com.example.fisintern_spring.services.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/reservations")
 public class ReservationController {
+    @Autowired
+    private ReservationRepository reservationRepository;
+
+    @Autowired
+    private ReservationService reservationService;
+
+    @GetMapping(path = "/all")
+    public @ResponseBody Iterable<Reservation> getAllUsers() {
+        return reservationRepository.findAll();
+    }
 }
