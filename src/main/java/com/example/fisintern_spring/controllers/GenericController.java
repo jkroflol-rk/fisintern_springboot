7fdf8bd2fb376e0e4590a357f70128e0347fd61f
J.K.Roflol <tori.tuan@gmail.com>
2024-05-05 16:24:11 +0700

    implement GenericController

---
+++ src/main/java/com/example/fisintern_spring/controllers/GenericController.java
@@ -0,0 +1,45 @@
+package com.example.fisintern_spring.controllers;
+
+import com.example.fisintern_spring.services.GenericService;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public abstract class GenericController<T, ID> {
+
+    private GenericService<T, ID> service;
+    private JpaRepository<T, ID> repository;
+    private String entityName;
+
+    public GenericController(GenericService<T, ID> service, JpaRepository<T, ID> repository,String entityName) {
+        this.service = service;
+        this.repository = repository;
+        this.entityName = entityName;
+    }
+
+    @GetMapping("/all")
+    public @ResponseBody List<T> getAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/findbyid/{id}")
+    public @ResponseBody T findById(@PathVariable ID id) {
+        return service.findById(id, entityName);
+    }
+
+    @PostMapping("/add")
+    public @ResponseBody T add(@RequestBody T entity) {
+        return repository.save(entity);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public @ResponseBody String delete(@PathVariable ID id) {
+        return service.deleteById(id, entityName);
+    }
+
+    @PutMapping("/update/{id}")
+    public @ResponseBody String update(@PathVariable ID id, @RequestBody T entity) {
+        return service.update(id, entity);
+    }
+}
--- src/main/java/com/example/fisintern_spring/controllers/ReservationController.java
+++ src/main/java/com/example/fisintern_spring/controllers/ReservationController.java
@@ -1,48 +1,31 @@
 package com.example.fisintern_spring.controllers;
 
 import com.example.fisintern_spring.models.Reservation;
-import com.example.fisintern_spring.models.Zone;
 import com.example.fisintern_spring.repositories.ReservationRepository;
-import com.example.fisintern_spring.repositories.ZoneRepository;
 import com.example.fisintern_spring.services.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping("/reservations")
-public class ReservationController {
+public class ReservationController extends GenericController<Reservation, Integer> {
     @Autowired
     private ReservationRepository reservationRepository;
 
     @Autowired
     private ReservationService reservationService;
-    @Autowired
-    private ZoneRepository zoneRepository;
 
-    @GetMapping(path = "/all")
-    public @ResponseBody Iterable<Reservation> getAllUsers() {
-        return reservationRepository.findAll();
+    public ReservationController(ReservationService reservationService, ReservationRepository reservationRepository) {
+        super(reservationService, reservationRepository, "Reservation");
     }
 
     @GetMapping(path = "/findbytime")
     public @ResponseBody Iterable<Reservation> findReservationsByTime(@RequestParam String t) {
         LocalDateTime time = LocalDateTime.parse(t);
         return reservationRepository.findReservationsByTime(time);
-    }
-
-    @PostMapping(path = "/add")
-    public @ResponseBody Reservation addNewReservation(@RequestBody Reservation reservation) {
-        return reservationRepository.save(reservation);
-    }
-
-    @DeleteMapping(path = "/delete/{id}")
-    public @ResponseBody String deleteReservation(@PathVariable int id) {
-        reservationRepository.deleteById(id);
-        return "Deleted reservation with id " + id + ".";
     }
 
     @PutMapping(path = "/updatestatus/{id}")
