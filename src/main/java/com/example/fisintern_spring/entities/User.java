ece3e08d6bdf68c23f87995539352e8f78451a3d
J.K.Roflol <tori.tuan@gmail.com>
2024-05-07 22:28:59 +0700

    remove leftover imports

--- src/main/java/com/example/fisintern_spring/entities/User.java
+++ src/main/java/com/example/fisintern_spring/entities/User.java
@@ -2,9 +2,6 @@ package com.example.fisintern_spring.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
