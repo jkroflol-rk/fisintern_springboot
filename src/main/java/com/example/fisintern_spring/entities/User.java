de0c3eff172e9bc10f316f6ab8a541860fc86ade
J.K.Roflol <tori.tuan@gmail.com>
2024-05-07 22:27:53 +0700

    remove security in main branch

--- src/main/java/com/example/fisintern_spring/entities/User.java
+++ src/main/java/com/example/fisintern_spring/entities/User.java
@@ -15,7 +15,7 @@ import java.util.List;
 @AllArgsConstructor
 @Entity
 @Table(name = "users")
-public class User implements UserDetails {
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,29 +34,4 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     @Column(name = "access")
     private Access access;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(access.name()));
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
